259a6af5e8deaf6414dcd8a4f724ff387d2d3a7d
AlexZheng <zc_work@outlook.com>
2021-02-14 12:50:21 +0800

    数据结构-数组： 数组中添加元素

--- 02-Array/src/Array.java
+++ 02-Array/src/Array.java
@@ -34,4 +34,37 @@ public class Array {
         return size == 0;
     }
 
+    //向数组末尾添加元素e
+    public void addLast(int e) {
+//        //判断一下数组空间是否能容器新加入的元素
+//        if (size==data.length){
+//            throw new IllegalArgumentException("AddLast Failed,array is full");
+//        }
+//        data[size] = e;
+//        size++;
+        add(size,e);
+    }
+
+    //向数组头部添加元素e
+    public void addFirst(int e){
+        add(0,e);
+    }
+
+    //向数组中index位置添加元素e
+    public void add(int index,int e){
+        if (size==data.length){
+            throw new IllegalArgumentException("Add Failed,array is full");
+        }
+
+        if (index < 0 || index > size){
+            throw new IllegalArgumentException("Add Failed,array require index >=0 or ibdex <= size");
+        }
+
+        for (int i = size - 1; i >= index; i--) {
+            data[i + 1] = data[i];
+        }
+        data[index] = e;
+        size++;
+    }
+
 }
