e8b400016224d65e85ab7ace7fc48db964347d1b
AlexZheng <zc_work@outlook.com>
2021-02-16 10:30:47 +0800

    数据结构-链表：使用链表实现栈并使用链表栈与数组栈进行性能对比。

---
+++ 03-Stack/src/LinkedList.java
@@ -0,0 +1,167 @@
+/**
+ * @Author Alex Zheng
+ * @Date 2021/2/15 18:48
+ * @Annotation
+ */
+public class LinkedList<E> {
+
+    private class Node{
+        public E e;
+        public Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this.e = e;
+            this.next = null;
+        }
+
+        public Node() {
+            this(null,null);
+        }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+    }
+
+    private Node dummyHead;
+    private int size;
+
+    public LinkedList(){
+        dummyHead = new Node(null,null);
+        size = 0;
+    }
+
+    //获取链表中元素个数
+    public int getSize(){
+        return size;
+    }
+
+    //返回链表是否为空
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    //在链表的index(0-based)位置添加新的元素e
+    //在链表中这不是一个常用的操作 练习用(考题、面试题、练习题)
+    public void add(int index,E e){
+        if (index < 0 || index > size){ //可以取到size，即在链表的尾部添加元素
+            throw new IllegalArgumentException("Add faile.Illegal index.");
+        }
+            Node prev = dummyHead;
+        //只需要遍历index次就好
+            for (int i = 0; i < index; i++) {
+                prev = prev.next;
+            }
+            prev.next = new Node(e,prev.next);
+            size++;
+    }
+
+    //在链表头添加新的元素
+    public void addFirst(E e){
+        add(0,e);
+    }
+
+    //向链表末尾添加新元素e
+    public void addLast(E e) {
+        add(size, e);
+    }
+
+    //获得链表的index(0-based)位置的元素e
+    //在链表中这不是一个常用的操作 练习用(考题、面试题、练习题)
+    public E get(int index){
+        if (index < 0 || index >= size){
+            throw new IllegalArgumentException("Get faile.Illegal index.");
+        }
+        Node current = dummyHead.next;
+        //get不需要index-1，因为我们要找的就是index节点
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.e;
+    }
+
+    //获取链表的第一个元素
+    public E getFirst() {
+        return get(0);
+    }
+
+    //获得链表最后一个元素
+    public E getLast() {
+        return get(size - 1);
+    }
+
+    //更新链表的index(0-based)位置的元素e
+    //在链表中这不是一个常用的操作 练习用(考题、面试题、练习题)
+    public void set(int index,E e){
+        if (index < 0 || index >= size){
+            throw new IllegalArgumentException("Set faile.Illegal index.");
+        }
+        Node current = dummyHead.next;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        current.e = e;
+    }
+
+    //查找链表中是否存在元素e
+    public boolean contains(E e){
+        Node current = dummyHead.next;
+        while (current!=null){
+            if (current.e.equals(e)){
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    //从链表中删除index(0-based)位置的元素，并返回删除的元素
+    //在链表中这不是一个常用的操作 练习用(考题、面试题、练习题)
+    public E remove(int index){
+        if (index < 0 || index >= size){
+            throw new IllegalArgumentException("Remove faile.Illegal index.");
+        }
+        Node prev = dummyHead;
+        //使用了dummyHead，故index不需要减一
+        for (int i = 0; i < index; i++) {
+            prev = prev.next;
+        }
+        Node delNode = prev.next;
+        prev.next = delNode.next; //prev.next = prev.next.next;
+        delNode.next = null;
+        size--;
+        return delNode.e;
+    }
+
+    //从链表中删除第一个元素
+    public E removeFirst() {
+        return remove(0);
+    }
+
+    //从链表中删除最后一个元素
+    public E removeLast() {
+        return remove(size - 1);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer();
+//        Node current = dummyHead.next;
+//        while (current!=null){
+//            sb.append(current + "->");
+//            current = current.next;
+//        }
+        for (Node current = dummyHead.next;current != null;current = current.next){
+            sb.append(current + "->");
+        }
+        sb.append("NULL");
+        return sb.toString();
+    }
+
+}
--- 03-Stack/src/Main.java
+++ 03-Stack/src/Main.java
@@ -1,3 +1,7 @@
+import com.sun.jmx.remote.internal.ArrayQueue;
+
+import java.util.Random;
+
 /**
  * @Author Alex Zheng
  * @Date 2021/2/14 17:50
@@ -5,12 +9,37 @@
  */
 public class Main {
 
-    public static void main(String[] args) {
-        ArrayStack<Integer> stack = new ArrayStack<>();
-        for (int i = 0; i < 10; i++) {
-            stack.push(i);
+    //使用Stack运行opCount个push和pop操作所需的事件，单位秒
+    private static double testStack(Stack<Integer> q,int opCount){
+
+        long start = System.nanoTime();
+
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++) {
+            q.push(random.nextInt(Integer.MAX_VALUE));
         }
-        System.out.println(stack);
+        for (int i = 0; i < opCount; i++) {
+            q.pop();
+        }
+
+        long end = System.nanoTime();
+
+        return (end-start)/1000000000.0;
+
+    }
+
+    public static void main(String[] args) {
+        int opCount = 10000000;
+
+        //二者都是O(n)，复杂度上看并没有太大差别
+        ArrayStack<Integer> arrayStack = new ArrayStack<>();
+        double time1 = testStack(arrayStack, opCount);
+        System.out.println("arrayStack: " + time1 + "s"); //arrayStack: 4.3886255s
+
+        //对于链表栈来说，opCount过多会导致每次都要new一个新的Node进而导致执行时间变长
+        LinkedListStack<Integer> linkedListStack = new LinkedListStack<>();
+        double time2 = testStack(linkedListStack, opCount);
+        System.out.println("linkedListStack: " + time2 + "s"); //linkedListStack: 4.5679196s
     }
 
 }
